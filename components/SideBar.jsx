7f5f4a5985846b93c55afaea133e27410cd7db5d
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-03-28 13:46:23 +0530

    bucket icon changed

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -88,13 +88,20 @@ export default function SideBar() {
           href="/bucket"
         >
           <svg
-            className=" w-5 h-5 text-gray-500 "
-            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokelinejoin="round"
+            className="lucide lucide-archive"
           >
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+            <rect width="20" height="5" x="2" y="3" rx="1" />
+            <path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8" />
+            <path d="M10 12h4" />
           </svg>
           <span className="mx-2  text-md font-medium ">Bucket</span>
         </Link>
