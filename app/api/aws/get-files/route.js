99e59c2e1ecd546202bfbe0230e305751403fe53
Sarthak Joshi <sarthakjoshi.cs18@gmail.com>
2024-05-07 22:16:31 +0530

    added pagination to images-gallery

--- app/api/aws/get-files/route.js
+++ app/api/aws/get-files/route.js
@@ -2,31 +2,48 @@ import { NextResponse } from "next/server";
 import AWS from "aws-sdk";
 
 export async function POST(request) {
-  const { store_name, folder_name } = await request.json();
-  console.log(store_name);
-
+  const {
+    store_name,
+    folder_name,
+    page = 1,
+    limit = 1000,
+  } = await request.json();
   const bucketName = "mazindabucket";
 
   try {
     const s3 = new AWS.S3();
-
     try {
-      // Use the listObjectsV2 function to list objects in the folder
-      const response = await s3
+      const startingIndex = (page - 1) * limit;
+
+      const response1 = await s3
         .listObjectsV2({
           Bucket: bucketName,
           Prefix: `${store_name}/${folder_name}`,
         })
         .promise();
 
-      // Extract the file names from the response
+      const response = await s3
+        .listObjectsV2({
+          Bucket: bucketName,
+          Prefix: `${store_name}/${folder_name}`,
+          MaxKeys: limit,
+          StartAfter:
+            startingIndex > 0
+              ? response1.Contents[startingIndex - 1].Key
+              : undefined,
+        })
+        .promise();
+
       const files = response.Contents.map((obj) => obj.Key);
 
-      // Return the list of files
+      const totalPages = Math.ceil(response1.KeyCount / limit);
+
       return NextResponse.json({
         success: true,
         message: "Files fetched successfully",
         files,
+        currentPage: page,
+        totalPages,
       });
     } catch (error) {
       console.error("Error retrieving files from S3:", error);
